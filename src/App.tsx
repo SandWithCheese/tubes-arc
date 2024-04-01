3dda584c3f06d9a0aa93142a03463a31114eb516
SandWithCheese <naufalahmad022@gmail.com>
2024-04-01 22:58:43 +0700

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <section className="flex flex-col items-center">
+      <h1>Hello World!</h1>
+      <Button>Click me!</Button>
+    </section>
+  )
+}
+
+export default App
