91525ae78e83415e4d6805a77d3ed6ff7d939ef4
AlthariqFairuz <althariqfairuz273@gmail.com>
2024-04-05 08:59:42 +0700

    feat: add about page

--- src/routes/about.lazy.tsx
+++ src/routes/about.lazy.tsx
@@ -6,8 +6,11 @@ export const Route = createLazyFileRoute("/about")({
 
 function About() {
   return (
-    <div className="p-6">
-      <h1>Hello from About!</h1>
+    <div className="flex flex-col justify-center items-center min-h-screen p-6" data-aos="fade-up">
+      <h1 className="font-bold text-primary text-5xl">About Us !</h1>
+      <p className="text-slate-200 text-2xl text-justify p-6" >Welcome to our API testing platform! We're here to simplify the process of testing and interacting with APIs.
+        Our platform offers a user-friendly interface for making GET and POST requests to APIs, allowing you to easily retrieve and send data.
+      </p>
     </div>
   )
 }
